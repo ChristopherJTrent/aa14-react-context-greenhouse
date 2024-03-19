6a1c37231a7e2bbc54c5db4a4bd7798e26b7d8cc
David <euntek95@gmail.com>
2024-03-19 12:47:14 -0700

    complete phase 2

--- src/components/Greenhouse/LightSwitch.jsx
+++ src/components/Greenhouse/LightSwitch.jsx
@@ -1,10 +1,18 @@
+import { useTheme } from '../../context/ThemeContext';
 import './LightSwitch.css';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme()
+  
+  const toggle = (e) => {
+    e.stopPropagation()
+    setThemeName(old => old === "day" ? "night" : "day") 
+  }
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className="light-switch day" onClick={toggle}>
+      <div className={themeName === "day" ? "on" : "off"}>DAY</div>
+      <div className={themeName === "night" ? "on" : "off"}>NIGHT</div>
     </div>
   );
 }
